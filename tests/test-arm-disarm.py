4a0a1b03f3294b8165a3400c627770c375cfca4d
shengje <35686590+shengje@users.noreply.github.com>
2018-01-22 22:59:07 +0800

    transfer python2 to python3

--- tests/test-arm-disarm.py
+++ tests/test-arm-disarm.py
@@ -6,7 +6,7 @@ __author__ = "Aldo Vargas"
 __copyright__ = "Copyright 2016 Altax.net"
 
 __license__ = "GPL"
-__version__ = "1"
+__version__ = "2"
 __maintainer__ = "Aldo Vargas"
 __email__ = "alduxvm@gmail.com"
 __status__ = "Development"
@@ -17,15 +17,15 @@ import time
 if __name__ == "__main__":
 
     #board = MultiWii("/dev/tty.usbserial-AM016WP4")
-    board = MultiWii("/dev/tty.SLAB_USBtoUART")
+    board = MultiWii("COM4")
     try:
         board.arm()
-        print "Board is armed now!"
-        print "In 3 seconds it will disarm..."
+        print ("Board is armed now!")
+        print ("In 3 seconds it will disarm...")
         time.sleep(3)
         board.disarm()
-        print "Disarmed."
+        print ("Disarmed.")
         time.sleep(3)
 
-    except Exception,error:
-        print "Error on Main: "+str(error)
+    except Exception as error:
+        print ("Error on Main: "+str(error))
